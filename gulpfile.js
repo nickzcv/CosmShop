2bc0f2888558234d7f52f26c25bb4eee5bcdfe88
Nick Z <nickzcv@gmail.com>
2016-12-21 22:32:36 +0300

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 var gulp = require("gulp"),//http://gulpjs.com/
-  util = require("gulp-util"),//https://github.com/gulpjs/gulp-util
-  sass = require("gulp-sass"),//https://www.npmjs.org/package/gulp-sass
-  minifycss = require('gulp-minify-css'),//https://www.npmjs.org/package/gulp-minify-css
-  rename = require('gulp-rename'),//https://www.npmjs.org/package/gulp-rename
-  log = util.log;
+    util = require("gulp-util"),//https://github.com/gulpjs/gulp-util
+    sass = require("gulp-sass"),//https://www.npmjs.org/package/gulp-sass
+    minifycss = require('gulp-clean-css'),//https://www.npmjs.org/package/gulp-minify-css
+    rename = require('gulp-rename'),//https://www.npmjs.org/package/gulp-rename
+    log = util.log;
 
 
 gulp.task("sass", function(){
