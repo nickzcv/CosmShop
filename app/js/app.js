fd32681765cd313dc5afff71736b06d22cf0b5af
Nick Z <nickzcv@gmail.com>
2016-12-24 13:25:31 +0300

    update base ui / mobile first

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,10 @@
 var app = app || {};
 
 $(function() {
-	var products = [
-		{ title: 'JavaScript: The Good Parts',
-			author: 'Douglas Crockford',
-			releaseDate: '2008',
-			keywords: 'JavaScript Programming' }
-	];
 
-	//new app.LibraryView(products);
+  // Mobile navigation handler (hamburger)
+  $('.hamburger').on('click', function() {
+    $('.navigation').toggleClass('hidden');
+  });
+
 });
