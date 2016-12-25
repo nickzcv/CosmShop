fa7fffbb5427384f39754a76f53bbe65c18a066b
Nick Z <nickzcv@gmail.com>
2016-12-25 08:29:02 +0300

    navigation update

--- app/js/app.js
+++ app/js/app.js
@@ -4,6 +4,7 @@ $(function() {
 
   // Mobile navigation handler (hamburger)
   $('.hamburger').on('click', function() {
+    $(this).toggleClass('closeBtn');
     $('.navigation').toggleClass('hidden');
   });
 
