67bd683a64e7988060eafd96f101387fb824b10a
Nick Z <nickzcv@gmail.com>
2017-01-08 17:21:19 +0300

    update cart popup

--- app/js/app.js
+++ app/js/app.js
@@ -4,6 +4,8 @@ $(function() {
 
   // Mobile navigation handler (hamburger)
   $('.hamburger').on('click', function() {
+    // hide cart if opened
+    $('.cart-popup').hide();
     $(this).toggleClass('closeBtn');
     $('.navigation').toggleClass('hidden');
   });
@@ -31,5 +33,29 @@ $(function() {
   $('.sub-nav').on('mouseleave', function() {
     $(this).fadeOut();
   });
+
+  // Cart popup handler
+  $('.cart').on({
+    mouseenter: function(){
+      var self = this;
+      clearTimeout(timer);
+      timer = setTimeout(function(){
+        $(self).find('.cart-popup').fadeIn();
+      }, 100)
+    },
+    mouseleave: function(){
+      var self = this;
+      setTimeout(function(){
+        if (!$(self).find('.cart-popup').is(":hover")){
+          $(self).find('.cart-popup').fadeOut();
+        }
+      }, 300);
+    }
+  });
+
+  $('.cart-popup').on('mouseleave', function() {
+    $(this).fadeOut();
+  });
+
 
 });
